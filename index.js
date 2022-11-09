7c7bc3664352160b6dddb59dbc1f1ab8c7d0953c
ryhorowitz <ryan.w.horowitz@gmail.com>
2022-11-09 15:42:38 -0500

    commit tree e495e82fc784f4c6305490e893e1525e6596e6d8

---
+++ index.js
@@ -0,0 +1,32 @@
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+ return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+function increment(n) {
+  return ++n
+}
+
+function decrement(n) {
+  return --n
+}
+
+function makeInt(n) {
+  const int = parseInt(n, 10)
+  return int
+}
+
+function preserveDecimal(n) {
+  return parseFloat(n);
+}
